71ff6e1edbf12f7fbb7632aee831bbee59a5232a
Zachary Blumenfeld <zblumenf@gmail.com>
2021-02-16 21:58:39 -0500

    Adding Common Package Unit Tests

--- src/test/java/com/zblumenf/spring/data/gremlin/common/TestConstants.java
+++ src/test/java/com/zblumenf/spring/data/gremlin/common/TestConstants.java
@@ -1,6 +1,18 @@
 package com.zblumenf.spring.data.gremlin.common;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestConstants {
+
+    public static final int DEFAULT_ENDPOINT_PORT = 443;
+    public static final int ILLEGAL_ENDPOINT_PORT = -1;
+    public static final String FAKE_ENDPOINT = "XXX-xxx.XXX-xxx.cosmosdb.azure.com";
+    public static final String FAKE_USERNAME = "XXX-xxx.username";
+    public static final String FAKE_PASSWORD = "XXX-xxx.password";
+    public static final String EMPTY_STRING = "";
+
     public static final String VERTEX_PERSON_LABEL = "label-person";
 
     public static final String VERTEX_PERSON_ID = "233333";
@@ -21,4 +33,6 @@ public class TestConstants {
     public static final String VERTEX_PROJECT_0_ID = "222222";
     public static final String VERTEX_PROJECT_0_NAME = "spring-data-documentdb";
     public static final String VERTEX_PROJECT_0_URI = "https://github.com/Microsoft/spring-data-documentdb";
+
+    public static final String VERTEX_SERVICE_LABEL = "label-service";
 }
